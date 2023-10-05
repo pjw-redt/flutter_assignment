1f261fd691e9739370438cf990c03c253cebfc95
pjw-redt <pjiww4@gmail.com>
2023-10-05 11:16:42 +0900

    1005_assign

--- lib/main.dart
+++ lib/main.dart
@@ -11,8 +11,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Assignment2',
+      title: 'Assignment3',
       theme: ThemeData(
+        primaryColor: Colors.blue,
+        primarySwatch: Colors.indigo,
         fontFamily: 'Pretendard',
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
@@ -27,65 +29,143 @@ class MyHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: AppColor.MyGreen,
-        titleTextStyle: const TextStyle(
-          color: Colors.white,
-          fontSize: 20.0,
-        ),
-        iconTheme: const IconThemeData(
-          color: Colors.white,
-        ),
-        title: const Text('Assignment2'),
-        actions: [
-          IconButton(onPressed: () {}, icon: Icon(Icons.add)),
-          IconButton(onPressed: () {}, icon: Icon(Icons.search)),
-          IconButton(onPressed: () {}, icon: Icon(Icons.person))
-        ],
-      ),
-      body: Column(
-        children: [
-          Stack(
-            alignment: Alignment.center,
-            children: [
-              Container(
-                  child: Opacity(
-                opacity: 0.2,
-                child: const Image(
-                    image: AssetImage('assets/warning.png'), width: 200.0),
-              )),
-              const Text(
-                'This page does not include contents',
-              )
-            ],
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.blue,
+          titleTextStyle: const TextStyle(
+            color: Colors.white,
+            fontSize: 20.0,
           ),
-          Expanded(
-              child: ListView(
-            children: [
-              ListTile(
-                leading: const Icon(Icons.home),
-                title: const Text('Home'),
-                trailing: const Icon(Icons.navigate_next),
-                onTap: () {},
-              ),
-              ListTile(
-                leading: const Icon(Icons.search),
-                title: const Text('Error Search'),
-                trailing: const Icon(Icons.navigate_next),
-                onTap: () {},
-              ),
-              ListTile(
-                leading: const Icon(Icons.refresh),
-                title: const Text('Refresh'),
-                trailing: const Icon(Icons.navigate_next),
-                onTap: () {},
-              ),
+          iconTheme: const IconThemeData(
+            color: Colors.white,
+          ),
+          title: const Text('Assignment3'),
+          actions: [
+            IconButton(onPressed: () {}, icon: Icon(Icons.add)),
+          ],
+          bottom: const TabBar(
+            tabs: [
+              Tab(icon: Icon(Icons.image)),
+              Tab(icon: Icon(Icons.how_to_vote)),
+              Tab(icon: Icon(Icons.upload)),
             ],
-          ))
-        ],
+          )
+          ),
+        body: TabBarView(
+          children: [
+            Tab(
+              child: GridView.count(
+                crossAxisCount: 3,
+                children: List.generate(16, (i) {
+                  return Container(
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage('assets/dog_images/dog_${i+1}.jpeg'),
+                        fit: BoxFit.contain,
+                      ),
+                    ),
+                  );
+                }),
+              ),
+            ),
+            Tab(
+              child: Column(
+                children: [
+                  Text('Cendidate', style: TextStyle(fontSize: 20.0)),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: Card(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10.0),
+                          ),
+                          elevation: 4.0,
+                          child: Container(
+                            width: 100,
+                            height: 100,
+                            child: Center(child: Text('Dog1', style: TextStyle(fontSize: 13.0, color: Colors.red))),
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                        child: Card(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10.0),
+                          ),
+                          elevation: 4.0,
+                          child: Container(
+                            width: 100,
+                            height: 100,
+                            child: Center(child: Text('Dog2', style: TextStyle(fontSize: 13.0, color: Colors.blue))),
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                        child: Card(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10.0),
+                          ),
+                          elevation: 4.0,
+                          child: Container(
+                            width: 100,
+                            height: 100,
+                            child: Center(child: Text('Dog3',
+                              style: TextStyle(fontSize: 13.0, color: Colors.green)),
+                          ),
+                        ),
+                      ),
+                      )],
+                  ),
+                  Text('Vote rate', style: TextStyle(fontSize: 20.0)),
+                  Row(
+                    children: [
+                      Expanded(
+                        flex: 5,
+                        child: Container(
+                          color: Colors.red,
+                          width: 100,
+                          height: 100,
+                          child: Center(child: Text('50%', style: TextStyle(fontSize: 13.0))),
+                        ),
+                      ),
+                      Expanded(
+                        flex: 2,
+                        child: Container(
+                          color: Colors.blue,
+                          width: 100,
+                          height: 100,
+                          child: Center(child: Text('20%', style: TextStyle(fontSize: 13.0))),
+                        ),
+                      ),
+                      Expanded(
+                        flex: 3,
+                        child: Container(
+                          color: Colors.green,
+                          width: 100,
+                          height: 100,
+                          child: Center(child: Text('30%', style: TextStyle(fontSize: 13.0))),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            Tab(
+              child: LinearProgressIndicator(),
+            ),
+          ],
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          items:[
+            BottomNavigationBarItem(icon: Icon(Icons.photo_album), label: 'album'),
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: 'home'),
+            BottomNavigationBarItem(icon: Icon(Icons.settings), label: 'settings'),
+          ]
+        )
       ),
-      drawer: Drawer(),
     );
   }
 }
